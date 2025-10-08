8c59bdf1d5a9e7aef7d0303509cedbf281424d39
Levi Fenton <levi.fenton@gmail.com>
2025-10-09 00:54:52 +1300

    finis

--- week7.c
+++ week7.c
@@ -989,3 +989,33 @@ void print_logo(int width) {
 
 //q25
 #include <stdio.h>
+
+void print_ascii_rectangle(char symbol, int width, int height);
+
+main() {
+
+    char symbol;
+    int width, height;
+
+    printf("Please enter an ASCII symbol:\n");
+    scanf(" %c", &symbol);
+    printf("Please enter the width:\n");
+    scanf("%d", &width);
+    printf("Please enter the height:\n");
+    scanf("%d", &height);
+
+    print_ascii_rectangle(symbol, width, height);
+
+    return 0;
+}
+
+void print_ascii_rectangle(char symbol, int width, int height) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%c", symbol);
+        }
+        printf("\n");
+    }
+}
+
+//finished
